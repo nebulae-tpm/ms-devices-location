f5bac032a236b8aa61ed8e4108144c54ecfebb52
Jaime Zapata <esteban.zapata@nebulae.com.co>
2018-05-09 13:47:20 -0500

    Adds log to check error

--- frontend/emi/devices-location/devices-location.component.ts
+++ frontend/emi/devices-location/devices-location.component.ts
@@ -54,7 +54,6 @@ export class DevicesLocationComponent implements OnInit, OnDestroy, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
     //Add '${implements OnChanges}' to the class.
-    console.log('ngOnChanges');
   }
 
   ngOnInit(): void {
