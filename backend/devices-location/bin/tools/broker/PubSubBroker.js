0562dcc52f1b328491bb0e8984f618e08fcfcd88
Jaime Zapata <esteban.zapata@nebulae.com.co>
2018-05-15 17:16:14 -0500

    Fix error on marker location

--- backend/devices-location/bin/tools/broker/PubSubBroker.js
+++ backend/devices-location/bin/tools/broker/PubSubBroker.js
@@ -209,7 +209,7 @@ class PubSubBroker {
                     senderId: this.senderId,
                     correlationId
                 }))
-            .do(messageId => console.log(`Message published through ${topic.name}, MessageId=${messageId}`, new Date()))
+            .do(messageId => console.log(`1Message published through ${topic.name}, MessageId=${messageId}`, new Date()))
             ;
     }
 
